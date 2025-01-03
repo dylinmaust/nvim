ecd8bc148661ae761b284fb9868006be458d5c0b
Dylin Maust <dylin.maust@gmail.com>
2025-01-03 11:45:47 -0500

    Fix Lazyvim root directory behavior
    Set root to cwd to avoid LSP overriding root directory

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,3 +1,6 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+-- https://www.hivefive.community/p/debugging-lazyvim-s-root-directory-behavior
+vim.g.root_spec = { "cwd" }
