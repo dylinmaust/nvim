4a9c8ccb9d4eb9d4bf909bd5c191d0b842674f35
Dylin Maust <dylin.maust@gmail.com>
2025-01-15 15:37:53 -0500

    Enable neotree, disable Neotree hotkeys

--- lua/plugins/disabled-plugins.lua
+++ lua/plugins/disabled-plugins.lua
@@ -1,3 +1,10 @@
 return {
-  { "nvim-neo-tree/neo-tree.nvim", enabled = false },
+  {
+    "nvim-neo-tree/neo-tree.nvim",
+    keys = {
+      { "<leader>e", false },
+      { "<leader>E", false },
+    },
+    enabled = true,
+  },
 }
